d5a942c4b3a16f9a65845316262587f476f334a9
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-08-30 13:16:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const main = document.getElementById("main-content");
+
+  const taskForm = document.getElementById("create-task-force");
+  const taskList = document.getElementById("tasks")
+
+  taskForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    const newTask = document.getElementById("new-task-description").value
+
+    const taskItem = document.createElement("li")
+    taskItem.innerText = newTask
+
+    taskList.appendChild(taskItem)
+  })
 });
